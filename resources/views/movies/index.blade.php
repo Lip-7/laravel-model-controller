417e603338c1d1ca75b27880255539b31d7b4a9e
Lip-7 <zhighofficial@gmail.com>
2023-05-23 15:59:19 +0200

    almost added all cards

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="container mt-5">
-    <h1 class="text-center mb-5 text-white">Our Productions</h1>
+    <h1 class="text-center moviesTitle mb-5 text-white">Our Productions</h1>
     <div class="cardwrap d-flex flex-wrap gap-3">
         @foreach ($movies as $movie)
             @include('movies.partials.movieCard', $movie)
