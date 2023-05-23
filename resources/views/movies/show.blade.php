695c6918db68f05c1af1298a43cecff16de23a27
Lip-7 <zhighofficial@gmail.com>
2023-05-23 17:55:40 +0200

    minor fixes

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
-<div class="showWrap">
-    <div class="row">
+<div class="showWrap my-5 container d-flex">
+    {{-- <div class="row">
         <div class="col-12 col-lg-6">
             <div class="imgWrap">
                 <img class="w-100" src="{{$image}}" alt="{{$title}}">
@@ -28,7 +28,53 @@
                 </div>
             </div>
         </div>
+    </div> --}}
+    <div class="imgwrap">
+        <img src="{{$image}}" alt="{{$title}}">
     </div>
+    <div class="movieInfo ps-4 mt-5 flex-grow-1">
+        <h1 class=" ms-5 mb-5">{{$title}}</h1>
+        <div class="title d-flex justify-content-start ms-5 gap-3 align-items-center my-4">
+            <h4>Original title: </h4>
+            <h3 class="text-uppercase">{{$original_title}}</h3>
+        </div>
+        <div class="nation d-flex justify-content-start ms-5 gap-3 align-items-center my-4">
+            <h4>Nationality: </h4>
+            <h3 class="text-uppercase">{{$nationality}}</h3>
+        </div>
+        <div class="published d-flex justify-content-start ms-5 gap-3 align-items-center my-4">
+            <h4>Published: </h4>
+            <h3 class="text-uppercase">{{$date}}</h3>
+        </div>
+        <div class="Vote d-flex justify-content-start ms-5 gap-3 align-items-center my-4">
+            <h4>Vote: </h4>
+            <h3 class="text-uppercase">{{$vote}}</h3>
+        </div>
+    </div>
+    {{-- <div class="imgwrap">
+        <img src="{{$image}}" alt="{{$title}}">
+    </div>
+    <div class="movieInfo mt-5 flex-grow-1">
+        <h1 class=" text-center mb-5">{{$title}}</h1>
+        <div class="title d-flex justify-content-center gap-3 align-items-center my-4">
+            <h4>Original title: </h4>
+            <h3 class="text-uppercase">{{$original_title}}</h3>
+        </div>
+        <div class="nation d-flex justify-content-center gap-3 align-items-center my-4">
+            <h4>Nationality: </h4>
+            <h3 class="text-uppercase">{{$nationality}}</h3>
+        </div>
+        <div class="published d-flex justify-content-center gap-3 align-items-center my-4">
+            <h4>Published: </h4>
+            <h3 class="text-uppercase">{{$date}}</h3>
+        </div>
+        <div class="Vote d-flex justify-content-center gap-3 align-items-center my-4">
+            <h4>Vote: </h4>
+            <h3 class="text-uppercase">{{$vote}}</h3>
+        </div>
+    </div> --}}
+
+
 </div>
 
 @endsection
